8a03ce8c9f33c1506f49d12657cd9ede42ae95cf
imironjin <cjfwls9806@gmail.com>
2024-06-20 22:02:51 +0900

    chore: add eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,55 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts", "src"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["react-refresh", "import"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-console": ["warn", { allow: ["warn", "error", "info"] }], // console.log() use warn
+    "no-unused-vars": "error", // 사용하지 않는 변수 error
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@/**",
+            group: "internal",
+          },
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["react*"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
   },
-}
+};
