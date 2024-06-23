e8ebdd87d0e65356b5ccfe0f6ef35608ac6335c0
imironjin <cjfwls9806@gmail.com>
2024-06-23 22:07:17 +0900

    feat: 로그인 페이지 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 
@@ -9,12 +10,24 @@ import App from './App.tsx';
 import '@/styles/reset.scss';
 import '@/index.scss';
 
+import NotFound from '@/pages/not-found/NotFound.tsx';
+import SignInPage from '@/pages/sign-in/SignInPage.tsx';
+
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ path: '/sign-in', element: <SignInPage /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
