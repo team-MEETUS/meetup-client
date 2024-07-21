76db27cec48dddfdb34b2f4be595f86574de4156
imironjin <cjfwls9806@gmail.com>
2024-07-21 21:35:38 +0900

    chore: 파일 정리

--- src/components/label/CardLabel.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './CardLabel.module.scss';
-
-const CardLabel = ({ label }: { label: string }) => {
-  return <span className={styles.label}>{label}</span>;
-};
-
-export default CardLabel;
