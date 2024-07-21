1e54782288b7be826a8c1c2012ee274f2b5f7a91
imironjin <cjfwls9806@gmail.com>
2024-07-21 21:49:54 +0900

    refactor: 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
 import './App.scss';
-import Header from '@/components/header/Header';
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="outlet-wrapper">
-        <Outlet />
-      </div>
+      <Outlet />
     </>
   );
 }
