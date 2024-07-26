1ccf39458d655ea14d232398d3b76d82dd59f914
imironjin <cjfwls9806@gmail.com>
2024-07-26 17:42:52 +0900

    feat: 불필요한 코드 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,34 +1,7 @@
 import styles from './HomePage.module.scss';
-import MeetingCard from '@/components/card/MeetingCard';
 
 const HomePage = () => {
-  const imageSrc = '/images/github.png';
-  const label = 'Sample Label';
-  const title = 'Sample Title';
-  const description = 'Sample description for the card component.';
-  const location = 'Sample Location';
-  const attendees = 20;
-
-  return (
-    <div className={styles.container}>
-      <MeetingCard
-        imageSrc={imageSrc}
-        label={label}
-        title={title}
-        description={description}
-        location={location}
-        attendees={attendees}
-      />
-      <MeetingCard
-        imageSrc={imageSrc}
-        label={'sample'}
-        title={title}
-        description={description}
-        location={location}
-        attendees={attendees}
-      />
-    </div>
-  );
+  return <div className={styles.container}>홈</div>;
 };
 
 export default HomePage;
