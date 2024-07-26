a24613b2341c7365c92e4aeb9af7d07a5dc81b98
imironjin <cjfwls9806@gmail.com>
2024-07-26 13:53:29 +0900

    feat: proxy 추가
    - api path로 시작하는 api url에 대하여 origin 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData:
-          '@import "./src/styles/common/_utils.scss"; @import "./src/styles/common/_typography.scss";  @import "./src/styles/common/variable.scss";',
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const API_URL = env.VITE_API_URL;
+
+  return defineConfig({
+    plugins: [svgr(), react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-});
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "./src/styles/common/_utils.scss"; @import "./src/styles/common/_typography.scss";  @import "./src/styles/common/variable.scss";',
+        },
+      },
+    },
+  });
+};
