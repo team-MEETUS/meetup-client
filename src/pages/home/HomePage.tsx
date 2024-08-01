e7b27e73ae1d976e078c1415169489b88ea1ea62
imironjin <cjfwls9806@gmail.com>
2024-08-01 23:05:42 +0900

    feat: 홈에 모임등록 버튼 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,16 @@
+import AddIcon from '@/assets/icons/AddIcon.svg?react';
+
 import styles from './HomePage.module.scss';
 
 const HomePage = () => {
-  return <div className={styles.container}>홈</div>;
+  return (
+    <div className={styles.container}>
+      홈
+      <button className={styles.button_container}>
+        <AddIcon />
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
