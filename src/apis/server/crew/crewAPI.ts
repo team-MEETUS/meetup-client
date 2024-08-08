453cbb29a19273f14edd1f8717bcf187728dbfac
imironjin <cjfwls9806@gmail.com>
2024-08-08 15:47:01 +0900

    feat: 모임 api 추가

--- src/apis/server/crew/crewAPI.ts
+++ src/apis/server/crew/crewAPI.ts
@@ -43,13 +43,30 @@ interface PutUpdateCrewAPI {
   interestSmall: number;
 }
 
-const GetAllCrewAPI = async () => {
-  const { data } =
-    await api.get<ApiResponse<GetCrewAPIResponseBody[]>>(`/crews`);
+interface GetCrewMemberAPIResponseBody {
+  crewMemberId: number;
+  status: number;
+  member: number;
+}
+/**
+ * @description 관심사 별 모임 조회
+ */
+const GetAllCrewAPI = async (params: {
+  city: string;
+  interestBigId: number;
+  interestSmallId: number;
+}) => {
+  const { data } = await api.get<ApiResponse<GetCrewAPIResponseBody[]>>(
+    `/crews`,
+    { params },
+  );
 
   return data;
 };
 
+/**
+ * @description 특정 모임 조회
+ */
 const GetCrewDetailAPI = async (crewId: number) => {
   const { data } = await api.get<ApiResponse<GetCrewAPIResponseBody[]>>(
     `/crews/${crewId}`,
@@ -58,20 +75,21 @@ const GetCrewDetailAPI = async (crewId: number) => {
   return data;
 };
 
-const PostAddCrewAPI = async (params: FormData) => {
+/**
+ * @description 모임 등록
+ */
+const PostAddCrewAPI = async (body: FormData) => {
   const { data } = await api.post<ApiResponse<{ crewId: number }>>(
     `/crews`,
-    params,
-    // {
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data',
-    //   },
-    // },
+    body,
   );
 
   return data;
 };
 
+/**
+ * @description 모임 수정
+ */
 const PutUpdateCrewAPI = async (crewId: number, params: PutUpdateCrewAPI) => {
   const { data } = await api.put<ApiResponse<{ crewId: number }>>(
     `/crews/${crewId}`,
@@ -81,9 +99,93 @@ const PutUpdateCrewAPI = async (crewId: number, params: PutUpdateCrewAPI) => {
   return data;
 };
 
+/**
+ * @description 모임 삭제
+ */
 const DeleteCrewAPI = async (crewId: number) => {
   const { data } = await api.delete<ApiResponse<HttpStatusCode>>(
     `/crews/${crewId}`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임 가입 신청
+ */
+const PostCrewMemberJoinAPI = async (crewId: number) => {
+  const { data } = await api.post<ApiResponse<HttpStatusCode>>(
+    `/crews/${crewId}`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임원 조회
+ */
+const GetCrewMemberAPI = async (crewId: number) => {
+  const { data } = await api.get<ApiResponse<GetCrewMemberAPIResponseBody[]>>(
+    `/crews/${crewId}/members`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임 찜 여부 조회
+ */
+const GetIsLikeCrewAPI = async (crewId: number) => {
+  const { data } = await api.get<ApiResponse<boolean>>(
+    `/crews/${crewId}/likes`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임 찜하기
+ */
+const PostLikeCrewAPI = async (crewId: number) => {
+  const { data } = await api.post<ApiResponse<{ crewLikeId: number }>>(
+    `/crews/${crewId}/likes`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임 찜 취소
+ */
+const DeleteLikeCrewAPI = async (crewId: number) => {
+  const { data } = await api.delete<ApiResponse<HttpStatusCode>>(
+    `/crews/${crewId}/likes`,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임원 권한 수정
+ */
+const PutUpdateCrewMemberAPI = async (
+  crewId: number,
+  body: { memberId: number; newRoleStatus: number },
+) => {
+  const { data } = await api.put<ApiResponse<{ memberId: number }>>(
+    `/crews/${crewId}/signup-members`,
+    body,
+  );
+
+  return data;
+};
+
+/**
+ * @description 모임 가입 신청 조회
+ */
+const GetCrewMemberJoinAPI = async (crewId: number) => {
+  const { data } = await api.get<ApiResponse<GetCrewMemberAPIResponseBody[]>>(
+    `/crews/${crewId}/signup-members`,
   );
 
   return data;
@@ -95,4 +197,11 @@ export {
   PostAddCrewAPI,
   PutUpdateCrewAPI,
   DeleteCrewAPI,
+  PostCrewMemberJoinAPI,
+  GetCrewMemberAPI,
+  GetIsLikeCrewAPI,
+  PostLikeCrewAPI,
+  DeleteLikeCrewAPI,
+  PutUpdateCrewMemberAPI,
+  GetCrewMemberJoinAPI,
 };
