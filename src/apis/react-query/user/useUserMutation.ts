907a98e25c503d98da8e189656e200c0e06401b3
imironjin <cjfwls9806@gmail.com>
2024-08-13 20:09:34 +0900

    feat: 로그인 타입 지정

--- src/apis/react-query/user/useUserMutation.ts
+++ src/apis/react-query/user/useUserMutation.ts
@@ -1,6 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
+import { ApiResponse } from '@/apis/server/type';
 import { PostLoginAPI } from '@/apis/server/user/userAPI';
 
 export const useUserMutation = () => {
@@ -9,12 +11,17 @@ export const useUserMutation = () => {
   const postLogin = useMutation({
     mutationFn: PostLoginAPI,
     onSuccess: (res) => {
-      const { success, data, error } = res;
+      const {
+        success,
+        data,
+        error,
+      }: ApiResponse<{ memberId: number; accessToken: string }> = res;
+
       if (success) {
         sessionStorage.setItem('ACCESS_TOKEN', data.accessToken);
         navigate('/');
       } else {
-        console.log(error);
+        toast.error(error?.message);
       }
     },
     onError: () => {},
