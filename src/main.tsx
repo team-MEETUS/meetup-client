9f0a8525d93365f0a02fceb44485066b70d5c19d
imironjin <cjfwls9806@gmail.com>
2024-08-13 20:09:04 +0900

    feat: toast 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import '@/styles/reset.scss';
 import '@/index.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
 import router from '@/router';
 
 const queryClient = new QueryClient();
@@ -15,6 +18,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
