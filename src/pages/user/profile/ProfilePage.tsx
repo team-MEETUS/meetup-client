3e9ee19fc49c2e1a93c2c72fb6cebb8dc6742a81
chaeyoon <chaeyoon010809@gmail.com>
2024-08-20 17:52:43 +0900

    design: 마이페이지 css 작성

--- src/pages/user/profile/ProfilePage.tsx
+++ src/pages/user/profile/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 
+import HomeHeader from '@/components/header/HomeHeader';
 import useUserStore from '@/stores/user/useUserStore';
 
 import styles from './ProfilePage.module.scss';
@@ -11,18 +12,26 @@ const ProfilePage = () => {
 
   return (
     <div className={cn('container')}>
-      {!memberId ? (
+      <div className={styles.header}>
+        <HomeHeader />
+      </div>
+      {memberId ? (
         <div>로그인이 필요합니다.</div>
       ) : (
-        <div>
-          <h2>프로필 페이지</h2>
-          <div>
+        <div className={cn('profile_item')}>
+          <div className={cn('profile_image')}>
             <img src="/images/profile-default.jpg" alt="프로필 이미지" />
-            <p>닉네임: 지니</p>
-            <p>회원 ID: 100</p>
-            <span>중계동</span>
-            <span>저는 짱이에요</span>
-            <span>2001.08.09</span>
+          </div>
+          <div className={cn('profile_info')}>
+            <div className={cn('profile_nickname')}>피카츄</div>
+            <div className={cn('profile_data')}>
+              <div className={cn('profile_city')}>서울특별시</div>
+              <span>·</span>
+              <div className={cn('profile_birth')}>2001.08.09</div>
+            </div>
+            <div className={cn('profile_intro')}>
+              피카츄님의 프로필 설명입니다. 귀여운 것이 특징!
+            </div>
           </div>
         </div>
       )}
