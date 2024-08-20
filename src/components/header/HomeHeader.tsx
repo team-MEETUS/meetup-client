ddd4c8c3e76fe5766be4e468b688c1ea933e1bd1
imironjin <cjfwls9806@gmail.com>
2024-08-20 23:04:30 +0900

    feat: 헤더에 지역 추가

--- src/components/header/HomeHeader.tsx
+++ src/components/header/HomeHeader.tsx
@@ -1,3 +1,5 @@
+import { toast } from 'react-toastify';
+
 import ChevronRightIcon from '@/assets/icons/ChevronRightIcon.svg?react';
 import useUserStore from '@/stores/user/useUserStore';
 
@@ -6,10 +8,13 @@ import styles from './HomeHeader.module.scss';
 const HomeHeader = () => {
   const { geo } = useUserStore();
 
+  const handleClickGeo = () => {
+    toast.info('지역 설정은 준비 중입니다.');
+  };
   return (
     <header className={styles.home_header}>
-      <div className={styles.header_left}>
-        {geo.district}
+      <div className={styles.header_left} onClick={handleClickGeo}>
+        {geo.district ? geo.district : '지역'}
         <ChevronRightIcon />
       </div>
     </header>
