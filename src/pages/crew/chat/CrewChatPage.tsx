245a897547cc0ca3bd52629f2147bed665fe7063
JH <middledown12@gmail.com>
2024-08-21 02:30:18 +0900

    fix: 채팅 실시간 통신 해결

--- src/pages/crew/chat/CrewChatPage.tsx
+++ src/pages/crew/chat/CrewChatPage.tsx
@@ -6,8 +6,8 @@ import CommonHeader from '@/components/header/CommonHeader';
 import axios from 'axios';
 
 interface ChatRespDto {
-  id: number;
   data: {
+    id: number;
     message: string;
     senderId: string;
     receiverId: string;
@@ -27,8 +27,8 @@ const ChatPage = () => {
 
   // Stomp의 CompatClient 객체를 참조하는 객체 (리렌더링에도 유지를 위해 useRef 사용)
   // Stomp라이브러리와 소켓 연결을 수행하는 cliet객체에 접근할 수 있게 해준다.
-  const clientRef = useRef<Client | null>(null);
-  const subscriptionRef = useRef<any>(null);
+  const clientRef: React.MutableRefObject<null> = useRef(null);
+  const subscriptionRef: React.MutableRefObject<null> = useRef(null);
   // const compatClient = useRef<CompatClient>(null);
 
   const [messages, setMessages] = useState<ChatRespDto[] | null>([]); // 채팅 기록
@@ -37,16 +37,24 @@ const ChatPage = () => {
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
+  // axios 기본 설정
+  axios.defaults.baseURL = 'http://localhost:8082';
+  axios.interceptors.request.use(
+    (config) => {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
   // 컴포넌트가 마운트될 때 connect 함수를 호출
   useEffect(() => {
     fetchMessages();
     connect();
     return () => {
-      if (clientRef.current) {
-        clientRef.current.deactivate();
-      }
-
-      setIsConnected(false);
+      disconnect();
       if (subscriptionRef.current) {
         subscriptionRef.current.unsubscribe();
         subscriptionRef.current = null;
@@ -58,31 +66,16 @@ const ChatPage = () => {
   const fetchMessages = async () => {
     let response: any;
     try {
-      axios.defaults.baseURL = 'http://localhost:8082';
-      console.log(token);
-      axios.interceptors.request.use(
-        (config) => {
-          config.headers['Authorization'] = `Bearer ${token}`;
-          return config;
-        },
-        (error) => {
-          // return Promise.reject(error);
-          return 'aaa';
-        },
-      );
-
       const params: any = {};
 
-      try {
-        if (receiverId) {
-          params.receiverId = receiverId; // receiverId가 있을 경우에만 추가
-          response = await axios.get(`/api/v1/crews/${crewId}/chats`, {
-            params,
-          });
-        } else {
-          response = await axios.get(`/api/v1/crews/${crewId}/chats`);
-        }
-      } catch (e: any) {}
+      if (receiverId) {
+        params.receiverId = receiverId; // receiverId가 있을 경우에만 추가
+        response = await axios.get(`/api/v1/crews/${crewId}/chats`, {
+          params,
+        });
+      } else {
+        response = await axios.get(`/api/v1/crews/${crewId}/chats`);
+      }
 
       if (Array.isArray(response.data)) {
         const validMessages = response.data.filter(
@@ -93,13 +86,10 @@ const ChatPage = () => {
         );
         setMessages(validMessages);
       } else {
-        console.error('Unexpected response format:', response.data);
+        console.error('예상치 못한 응답 형식:', response.data);
       }
     } catch (error: any) {
-      console.log(response);
-      if (response.data == null) {
-        setError('채팅 내역이 없습니다');
-      }
+      console.error('메시지 가져오기 오류:', error);
     }
   };
 
@@ -139,7 +129,7 @@ const ChatPage = () => {
     console.log('WebSocket 연결 성공');
     if (!subscriptionRef.current) {
       setIsConnected(true);
-      subscriptionRef.current = clientRef.current?.subscribe(
+      subscriptionRef.current = clientRef.current.subscribe(
         '/topic/messages',
         onMessageReceived,
       );
@@ -150,11 +140,11 @@ const ChatPage = () => {
     const chatMessage = JSON.parse(message.body);
     if (chatMessage && chatMessage.data !== undefined) {
       setMessages((prevMessages) => {
-        const isDuplicate = prevMessages?.some(
-          (msg) => msg.id === chatMessage.id,
+        const isDuplicate = prevMessages.some(
+          (msg) => msg.data.id === chatMessage.id,
         );
         if (!isDuplicate) {
-          return prevMessages !== null ? [...prevMessages, chatMessage] : null;
+          return [...prevMessages, chatMessage];
         }
         return prevMessages;
       });
@@ -164,23 +154,32 @@ const ChatPage = () => {
   // 소켓을 통해 메시지를 전송
   const sendMessage = useCallback(() => {
     console.log(senderId);
+
     // client.current가 존재하고 연결되었다면 메시지 전송
     if (isConnected && clientRef.current && message.trim()) {
+      const chatMessage = { senderId, receiverId, message, crewId };
       clientRef.current.publish({
         destination: `/app/send/${crewId}`,
-        body: JSON.stringify({
-          senderId: senderId,
-          receiverId: receiverId,
-          message,
-          crewId: crewId,
-        }),
+        body: JSON.stringify(chatMessage),
       });
+      setMessage('');
+    } else {
+      console.error(
+        '클라이언트가 연결되어 있지 않거나 메시지가 비어 있습니다.',
+      );
     }
   }, [isConnected, message, senderId, receiverId, crewId]);
 
   const backIconClick = () => {
     sessionStorage.clear();
     navigate(-1);
+  };
+
+  const disconnect = () => {
+    if (clientRef.current) {
+      clientRef.current.deactivate();
+    }
+    setIsConnected(false);
   };
 
   return (
