fb1c279a532145a9d5b7f96fba73989df3471aac
chaeyoon <chaeyoon010809@gmail.com>
2024-08-21 21:39:53 +0900

    feat: 상세 관심사 별 모임 조회

--- src/pages/crew/crew-interest/CrewInterestBigPage.tsx
+++ src/pages/crew/crew-interest/CrewInterestBigPage.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import { useCrewListQuery } from '@/apis/react-query/crew/useCrewQuery';
+import { useInterestSmallQuery } from '@/apis/react-query/interest/useInterestQuery';
 import CrewAddIcon from '@/assets/icons/CrewAddIcon.svg?react';
 import CrewCard from '@/components/common/crew-card/CrewCard';
 import CommonHeader from '@/components/header/CommonHeader';
@@ -14,22 +15,66 @@ const CrewInterestBigPage = () => {
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
-  const interestId = queryParams.get('interestId');
+  const interestBigId = queryParams.get('interestBigId');
+  const interestSmallId = queryParams.get('interestSmallId');
   const name = queryParams.get('name');
 
   const { data: interestData } = useCrewListQuery({
-    interestBigId: Number(interestId),
+    interestBigId: Number(interestBigId),
+    ...(interestSmallId !== null && {
+      interestSmallId: Number(interestSmallId),
+    }),
     page: 1,
   });
 
+  const { data: interestSmallData } = useInterestSmallQuery(
+    Number(interestBigId) || 0,
+  );
+
+  const handleInterestBigClick = (interestBigId: number, name: string) => {
+    navigate(`/crew?interestBigId=${interestBigId}&name=${name}`);
+  };
+
+  const handleInterestSmallClick = (interestSmallId: number, name: string) => {
+    navigate(
+      `/crew?interestBigId=${interestBigId}&interestSmallId=${interestSmallId}&name=${name}`,
+    );
+  };
+
   const handleClickBack = () => {
-    navigate(-1);
+    navigate('/');
   };
 
   return (
     <div className={cn('container')}>
       <div className={cn('header')}>
         <CommonHeader title={name} onBackClick={handleClickBack} />
+      </div>
+
+      {/* 상세 관심사 선택 */}
+      <div className={styles.interest_list}>
+        <div
+          className={styles.interest_item}
+          onClick={() =>
+            handleInterestBigClick(
+              Number(interestBigId),
+              interestData[0].interestBig.name,
+            )
+          }
+        >
+          <span className={cn('interest_name')}>전체</span>
+        </div>
+        {interestSmallData?.map((interest) => (
+          <div
+            key={interest.interestSmallId}
+            className={styles.interest_item}
+            onClick={() =>
+              handleInterestSmallClick(interest.interestSmallId, interest.name)
+            }
+          >
+            <span className={cn('interest_name')}>{interest.name}</span>
+          </div>
+        ))}
       </div>
 
       {/* 관심사 별 모임 */}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -253,7 +253,7 @@ const router = createBrowserRouter([
             ),
           },
           {
-            path: ':interestBig',
+            path: '',
             element: (
               <Suspense fallback={<LoadingSpinner />}>
                 <CrewInterestBigPage />
