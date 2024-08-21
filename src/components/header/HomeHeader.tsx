54bc353facdea04b8e9eeb0266948f72ebcaca56
imironjin <cjfwls9806@gmail.com>
2024-08-22 00:57:20 +0900

    feat: 알림 임시 제거

--- src/components/header/HomeHeader.tsx
+++ src/components/header/HomeHeader.tsx
@@ -1,18 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useEffect, useMemo, useState } from 'react';
 
 import { useQueryClient } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
-import { EventSourcePolyfill } from 'event-source-polyfill';
 import { toast } from 'react-toastify';
 
 import { useNotificationListQuery } from '@/apis/react-query/crew/useHomeQuery';
 import ChevronRightIcon from '@/assets/icons/ChevronRightIcon.svg?react';
 import NotificationButton from '@/components/common/notification-button/NotificationButton';
 import useUserStore from '@/stores/user/useUserStore';
-import { GetSubscribedNotificationResponseBody } from '@/types/home/homeAPIType';
-import { sanitizeHTML } from '@/utils/sanitizeHTML';
 
 import styles from './HomeHeader.module.scss';
 
@@ -23,7 +18,7 @@ const HomeHeader = ({ type = 'default' }: HomeHeaderProps) => {
   const cn = classNames.bind(styles);
   const queryClient = useQueryClient();
 
-  const [notification, setNotification] = useState<number>(0);
+  const [notification] = useState<number>(0);
 
   const VITE_API_URL = useMemo(() => import.meta.env.VITE_API_URL, []);
 
@@ -43,36 +38,34 @@ const HomeHeader = ({ type = 'default' }: HomeHeaderProps) => {
   );
 
   useEffect(() => {
-    if (!ACCESS_TOKEN) return;
-    const connect = () => {
-      const eventSource = new EventSourcePolyfill(
-        `${VITE_API_URL}/notifications/subscribe`,
-        {
-          headers: {
-            Authorization: `Bearer ${ACCESS_TOKEN}`,
-            'Content-Type': 'text/event-stream',
-          },
-          heartbeatTimeout: 86400000,
-        },
-      );
-
-      eventSource.addEventListener('notification', (e) => {
-        const data: GetSubscribedNotificationResponseBody = JSON.parse(e.data);
-        if (!data.message) {
-          setNotification(data.notificationCount);
-        } else {
-          toast.info(sanitizeHTML(data.message));
-        }
-      });
-
-      eventSource.onerror = () => {
-        eventSource.close();
-      };
-      return () => {
-        eventSource.close();
-      };
-    };
-    return connect();
+    // if (!ACCESS_TOKEN) return;
+    // const connect = () => {
+    //   const eventSource = new EventSourcePolyfill(
+    //     `${VITE_API_URL}/notifications/subscribe`,
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${ACCESS_TOKEN}`,
+    //         'Content-Type': 'text/event-stream',
+    //       },
+    //       heartbeatTimeout: 86400000,
+    //     },
+    //   );
+    //   eventSource.addEventListener('notification', (e) => {
+    //     const data: GetSubscribedNotificationResponseBody = JSON.parse(e.data);
+    //     if (!data.message) {
+    //       setNotification(data.notificationCount ?? 0);
+    //     } else {
+    //       toast.info(sanitizeHTML(data.message));
+    //     }
+    //   });
+    //   eventSource.onerror = () => {
+    //     eventSource.close();
+    //   };
+    //   return () => {
+    //     eventSource.close();
+    //   };
+    // };
+    // return connect();
   }, [VITE_API_URL, ACCESS_TOKEN, queryClient]);
 
   return (
